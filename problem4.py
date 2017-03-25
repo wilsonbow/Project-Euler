ebe7843874deecabb50a585d675f99880d8d5eae
Wilson Bow <wilson.bow@my.jcu.edu.au>
2017-03-25 19:59:10 +1000

    Completed problem 4

--- problem4.py
+++ problem4.py
@@ -1,10 +1,10 @@
 """
 Project Euler Problem 4
 Find the largest palindrome made from the product of two three digit numbers.
+
+Correct answer found using this program.
 """
 
-# Let the two numbers be a and b
-# Increase them one at a time and check product
 
 def is_palindrome(number_str):
     reverse_str = number_str[::-1]
@@ -15,4 +15,13 @@ def is_palindrome(number_str):
         return False
 
 
-print(is_palindrome('bob'))
+# Let the two numbers be a and b
+# Increase them one at a time and check product
+largest_palindrome = 0
+for a in range(100, 1000):
+    for b in range(a, 1000):
+        c = a * b
+        if is_palindrome(str(c)) and c > largest_palindrome:
+            largest_palindrome = c
+
+print("The largest palindrome created as a product of two three-digit numbers is: {}".format(largest_palindrome))
