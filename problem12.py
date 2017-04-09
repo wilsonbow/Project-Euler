c51413d92b895ed7f9a351f978acb9361b5d5be0
Wilson Bow <wilson.bow@my.jcu.edu.au>
2017-04-09 18:13:29 +1000

    Solved Problem 12

--- problem12.py
+++ problem12.py
@@ -1,6 +1,8 @@
 """
 Project Euler Problem 12
 Find first triangular number to have more than 500 divisors
+
+Solved
 """
 
 from math import sqrt
@@ -10,8 +12,9 @@ def find_num_divisors(number):
     num_divisors = 0
     for divisor in range(1, int(sqrt(number))+1):
         if number % divisor == 0:
-            num_divisors += 2
+            num_divisors += 2 # divisors exist in pairs
 
+    # two divisors were counted for the square root
     if sqrt(number) % 1 == 0:
         num_divisors -= 1
 
@@ -27,7 +30,7 @@ while not finished:
     num_divisors = find_num_divisors(number)
     print("{:10}{:4}".format(number, num_divisors))
 
-    if num_divisors >= 10:
+    if num_divisors >= 500:
         finished = True
 
 print("The first triangular number with over 500 divisors is: {}".format(number))
