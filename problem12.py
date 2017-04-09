51946e2a123bf306d6da09078c390973dce02031
Wilson Bow <wilson.bow@my.jcu.edu.au>
2017-04-09 18:10:56 +1000

    Optimised algorithm to check up to square root of number rather than half.

--- problem12.py
+++ problem12.py
@@ -3,12 +3,17 @@ Project Euler Problem 12
 Find first triangular number to have more than 500 divisors
 """
 
+from math import sqrt
+
 
 def find_num_divisors(number):
     num_divisors = 0
-    for divisor in range(1, int(number/2+1)):
+    for divisor in range(1, int(sqrt(number))+1):
         if number % divisor == 0:
-            num_divisors += 1
+            num_divisors += 2
+
+    if sqrt(number) % 1 == 0:
+        num_divisors -= 1
 
     return num_divisors
 
@@ -20,9 +25,9 @@ while not finished:
     additive += 1
     number += additive
     num_divisors = find_num_divisors(number)
-    print("{:10}{:3}".format(number, num_divisors))
+    print("{:10}{:4}".format(number, num_divisors))
 
-    if num_divisors >= 500:
+    if num_divisors >= 10:
         finished = True
 
 print("The first triangular number with over 500 divisors is: {}".format(number))
